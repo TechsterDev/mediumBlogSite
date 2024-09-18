2bb89c5180fa3ec7a6dc1ab918f5dce0362e6a8d
indrakumar <indrakumarhn01@gmail.com>
2024-09-18 23:01:47 +0530

    added the common dir

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import {Hono} from "hono"
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
+import { signupInput } from "../zod"
 
 export const userRouter = new Hono<{
     Bindings: {
@@ -11,6 +12,7 @@ export const userRouter = new Hono<{
   }>()
 userRouter.post('/signup', async (c) => {
     const body=await c.req.json();
+    const {success}=signupInput.safeParse(body);
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
